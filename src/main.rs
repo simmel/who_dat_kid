830f1f063bfab0c2c37cbf5ac5bfadce0f61dea4
Simon Lundström <github-commits@soy.se>
2020-03-14 07:24:09 +0000

    Add some more protocol specifics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn show_fake_id(ident_request: &String) -> String {
+  let error = String::from(":ERROR:UNKNOWN-ERROR\r\n");
+  return error;
+}
+
 fn main() {
-  println!("Hello, world!");
+  let request = String::from("13,37\r\n");
+  let reply = show_fake_id(&request);
+  println!("{}", reply);
 }
