088f767f42954791e1ef6f03ab542becf22d74ec
Simon Lundström <github-commits@soy.se>
2020-03-15 15:08:19 +0000

    Add a simple test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ fn show_fake_id(ident_request: &String) -> String {
   return error;
 }
 
+#[test]
+fn bogus_request() {
+  let reply = show_fake_id(&String::from("GURKEN"));
+  assert_eq!(reply, String::from(":ERROR:UNKNOWN-ERROR\r\n"));
+}
+
 fn main() {
   let request = String::from("13,37\r\n");
   let reply = show_fake_id(&request);
