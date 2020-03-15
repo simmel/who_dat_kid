653058adba36689bba8dc20ad644661049a4018f
Simon Lundström <github-commits@soy.se>
2020-03-15 15:11:23 +0000

    Make the error message a constant

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+const IDENT_ERROR: &str = ":ERROR:UNKNOWN-ERROR\r\n";
+
 fn show_fake_id(ident_request: &String) -> String {
-  let error = String::from(":ERROR:UNKNOWN-ERROR\r\n");
+  let error = String::from(IDENT_ERROR);
   return error;
 }
 
 #[test]
 fn bogus_request() {
   let reply = show_fake_id(&String::from("GURKEN"));
-  assert_eq!(reply, String::from(":ERROR:UNKNOWN-ERROR\r\n"));
+  assert_eq!(reply, String::from(IDENT_ERROR));
 }
 
 fn main() {
