61302fd1a1ce41a9cfdaac66594233f2cc128ef9
Simon Lundström <github-commits@soy.se>
2020-03-15 15:21:58 +0000

    Add a correct test (which fails)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 const IDENT_ERROR: &str = ":ERROR:UNKNOWN-ERROR\r\n";
 
+// FIXME: Add get ports from request
 fn show_fake_id(ident_request: &String) -> String {
   let error = String::from(IDENT_ERROR);
   return error;
@@ -9,6 +10,12 @@ fn show_fake_id(ident_request: &String) -> String {
 fn bogus_request() {
   let reply = show_fake_id(&String::from("GURKEN"));
   assert_eq!(reply, String::from(IDENT_ERROR));
+}
+
+#[test]
+fn correct_request() {
+  let reply = show_fake_id(&String::from("13,37\r\n"));
+  assert_eq!(reply, String::from("13,37:USERID:UNIX:aerokid"));
 }
 
 fn main() {
